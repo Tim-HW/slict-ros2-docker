0203d69ea7e3135be5fa8ace63548a9f45297c17
Tim Desktop <timothee.freville@mil.be>
2025-03-06 18:27:14 +0100

    long day

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # List of container names to remove
-containers=("ros1_bridge" "slict")
+containers=("slictdocker")
+DOCKER_IMG=slictdocker
 
 # Loop through each container name and remove it
 for container in "${containers[@]}"; do
     docker rm -f "$container"
 done
 
-docker-compose up
+docker run \
+        -it \
+        --rm \
+        --net=host \
+        -v ./data:/root/dataset/ \
+        -e USER=root \
+        $DOCKER_IMG \
+        bash
+
