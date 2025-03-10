7ae71c8dbf1a8fd1ddb6f34ee720b14e5abc7304
Tim Desktop Linux <timothee.freville@mil.be>
2025-03-10 16:33:22 +0100

    working from scratch

--- run.sh
+++ run.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
+# Start Docker containers in detached mode
 docker-compose up -d
 
-docker exec slict /bin/bash -c 'source /ws_slam/devel/setup.bash && roslaunch slict run_slam.launch'
+# Wait for containers to be fully up (adjust sleep time as needed)
+sleep 5
 
+# Start SLAM process inside the 'slict' container
+docker exec slict /bin/bash -c 'source /ws_slam/devel/setup.bash && roslaunch slict run_em2c.launch' &
 
+# Start ROS1-ROS2 bridge inside the 'ros1_bridge' container
+docker exec ros1_bridge /bin/bash -c '. /opt/ros/foxy/setup.bash && \
+                                     . /opt/ros/noetic/setup.bash && \
+                                     . /ROS2/install/setup.bash && \
+                                     . /ROS1/devel/setup.bash && \
+                                     . /bridge_ws/install/setup.bash && \
+                                     ros2 run ros1_bridge dynamic_bridge --bridge-all-topics' &
 
-#docker exec ros1_bridge /bin/bash -c '. /opt/ros/foxy/setup.bash && . /opt/ros/noetic/setup.bash  &&. /ROS2/install/setup.bash && . /ROS1/devel/setup.bash && . /bridge_ws/install/setup.bash && ros2 run ros1_bridge dynamic_bridge --bridge-all-topics'
-
+# Wait for background processes to finish
+wait
